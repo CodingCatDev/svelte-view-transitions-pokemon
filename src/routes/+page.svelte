96fb435bea42c38dc739e6b7bc6d5ce140a30f58
Alex Patterson <alex@codingcat.dev>
2024-02-05 13:39:43 -0500

    using library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,84 @@
 <script lang="ts">
 	export let data;
-	import { onNavigate } from '$app/navigation';
+	import { setupViewTransition } from 'sveltekit-view-transition';
 
-	onNavigate((props) => {
-		return new Promise((resolve) => {
-			//@ts-ignore
-			document.startViewTransition(async () => {
-				resolve();
-				await props.complete;
-			});
-		});
-	});
+	const { transition, classes } = setupViewTransition();
 </script>
 
-<ul>
-	{#each data?.pokemon as pokemon}
-		<li>
-			<a class="underline inline" href={`/${pokemon.name}`}>{pokemon.name}</a>
-			<img src={pokemon?.sprites?.front_default} alt={pokemon?.name} />
-		</li>
-	{/each}
-</ul>
+<section>
+	<ul>
+		{#each data?.pokemon as pokemon}
+			<li>
+				<a
+					use:transition={{
+						name: 'sprite-name',
+						shouldApply: (params) => {
+							return params.navigation.to?.params?.id === pokemon.name;
+						},
+						applyImmediately: (params) => {
+							return params.isInViewport && params.navigation.from?.params?.id === pokemon.name;
+						}
+					}}
+					class="underline inline"
+					style="display: block;"
+					href={`/${pokemon.name}`}
+					>{pokemon.name}
+				</a>
+				<img
+					use:transition={{
+						name: 'sprite',
+						shouldApply: (params) => {
+							return params.navigation.to?.params?.id === pokemon.name;
+						},
+						applyImmediately: (params) => {
+							return params.isInViewport && params.navigation.from?.params?.id === pokemon.name;
+						}
+					}}
+					src={pokemon?.sprites?.front_default}
+					alt={pokemon?.name}
+				/>
+			</li>
+		{/each}
+	</ul>
+</section>
 
 <style>
+	section {
+		background: tomato;
+	}
+	@keyframes slide {
+		from {
+			transform: translateX(var(--from));
+		}
+		to {
+			transform: translateX(var(--to));
+		}
+	}
 	:root::view-transition-new(root),
 	:root::view-transition-old(root) {
-		animation-duration: 5s;
+		animation: slide 1s linear;
+	}
+	:root::view-transition-group(sprite) {
+		animation-duration: 1s;
+	}
+	:root::view-transition-group(sprite-name) {
+		animation-duration: 1s;
+	}
+
+	:root::view-transition-new(root) {
+		--from: 100%;
+		--to: 0%;
+	}
+	:root::view-transition-old(root) {
+		--from: 0%;
+		--to: -100%;
+	}
+	.back:root::view-transition-new(root) {
+		--from: -100%;
+		--to: 0%;
+	}
+	.back:root::view-transition-old(root) {
+		--from: 0%;
+		--to: 100%;
 	}
 </style>
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,7 +1,27 @@
 <script lang="ts">
 	export let data;
+	import { setupViewTransition } from 'sveltekit-view-transition';
+
+	const { transition, classes } = setupViewTransition();
+	classes((nav) => {
+		console.log('id', nav.navigation.to?.route.id);
+		if (nav.navigation.to?.route.id !== '/') {
+			return ['back'];
+		}
+	});
 	$: sprites = data?.pokemon?.sprites;
 </script>
 
-<h1>{data?.pokemon?.name}</h1>
-<img src={sprites?.front_default} alt={data?.pokemon?.name} />
+<section>
+	<a href="/">Home</a>
+	<h1 use:transition={'sprite-name'}>
+		{data?.pokemon?.name}
+	</h1>
+	<img use:transition={'sprite'} src={sprites?.front_default} alt={data?.pokemon?.name} />
+</section>
+
+<style>
+	section {
+		background: lightgreen;
+	}
+</style>
